0d1692f8bda63c724770a3874053874f4ab33e5d
Phil Lachmann <phil.lachmann@gmail.com>
2026-07-06 19:47:55 -0400

    Layer .env.development.local into drizzle-kit config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
 config({ path: ".env.local" });
+config({ path: ".env.development.local", override: true });
 
 export default defineConfig({
   schema: "./src/lib/db/schema.ts",
